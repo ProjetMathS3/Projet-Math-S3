0d3eaf18ab876927c16a9af55f881f9384add3f8
r14006780 <rromain27@neuf.fr>
2015-10-15 10:15:55 +0200

    bonjour

--- core/Proie.java
+++ core/Proie.java
@@ -2,6 +2,10 @@ package core;
 
 import graphe.AfficheFonction;
 
+import utils.Case;
+import utils.Directions;
+
+import java.awt.*;
 import java.util.ArrayList;
 
 /**
@@ -9,26 +13,18 @@ import java.util.ArrayList;
  */
 public class Proie extends Espece {
 
-    /*PREDATEURPRESENT - Comportement:
-      Renvoie TRUE uniquement si au moins un prédateur est dans le champs de vision (getVision) de la proie
-     */
-    private boolean isPredateurPresent (){
-        //todo
-    } //predateurPresent
-
-
-    /*CONGENERELEPLUSPROCHE - Comportement:
-      Parcourt le tableau d'espèce congénère et retourne le point du congénère le plus proche
+    /**OUFUIR - Comportement:
+     * Définit un point opposé à la position du predateur le plus proche
      */
     private Point ouFuir () {
         //todo
     } //congenereLePlusProche
 
 
-    /*FUIR - Comportement:
-      La proie regarde dans son champs de vision (getVision) si il y a un ou plusieurs prédateurs
-      SI c'est le cas, ALORS elle se déplace dans la direction opposé au prédateur le plus proche
-      SINON, elle ne fait rien
+    /**FUIR - Comportement:
+     * La proie regarde dans son champs de vision (getVision) si il y a un ou plusieurs prédateurs
+     * SI c'est le cas, ALORS elle se déplace dans la direction opposé au prédateur le plus proche
+     * SINON, elle ne fait rien
      */
     private void fuir(ArrayList <Espece> Predateur) {
         if (trouverIndividuProche(Predateur) != null) {
@@ -36,19 +32,19 @@ public class Proie extends Espece {
         }
     } //fuir ()
 
-    /*JOUERTOUR - Comportement:
-      La proie se reproduit si une autre est directement dans son entourage
-      SI un prédateur ou plus est dans son champs de vision,
-      ALORS la proie fuit,
-      SINON SI une autre proie ou plus est dans son champs de vision,
-      ALORS elle va vers elle pour se reproduir,
-      SINON, elle ne fait rien
+    /**JOUERTOUR - Comportement:
+     * La proie se reproduit si une autre est directement dans son entourage
+     * SI un prédateur ou plus est dans son champs de vision,
+     * ALORS la proie fuit,
+     * SINON SI une autre proie ou plus est dans son champs de vision,
+     * ALORS elle va vers elle pour se reproduir,
+     * SINON, elle ne fait rien
      */
-    public void jouerTour() {
+    public void jouerTour(ArrayList <Espece> Proie, ArrayList <Espece> Predateur) {
         this.age += 1;
-        seReproduire ();
-        fuir();
-        allerVersCongenere();
+        seReproduire (Proie);
+        fuir(Predateur);
+        allerVersCongenere(Proie);
     } //jouerTour ()
 
 }
