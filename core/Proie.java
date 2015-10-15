917dcc1d9baa5bd33ecec7fed0220fea0f461097
r14006780 <rromain27@neuf.fr>
2015-10-15 09:35:55 +0200

    BONSOUAGUE

--- core/Proie.java
+++ core/Proie.java
@@ -1,11 +1,14 @@
 package core;
 
+import java.awt.*;
+import java.util.ArrayList;
+
 /**
  * Created by r14003530 on 09/10/15.
  */
 public class Proie extends Espece {
 
-    /*PREDATEURPRESENT - Comportement:
+    /**ISPREDATEURPRESENT - Comportement:
       Renvoie TRUE uniquement si au moins un prédateur est dans le champs de vision (getVision) de la proie
      */
     private boolean isPredateurPresent (){
@@ -13,45 +16,37 @@ public class Proie extends Espece {
     } //predateurPresent
 
 
-    /*CONGENERELEPLUSPROCHE - Comportement:
-      Parcourt le tableau d'espèce congénère et retourne le point du congénère le plus proche
+    /**OUFUIR - Comportement:
+     * La proie va à l'opposé du prédateur le plus proche trouvé dans la limite de ses points de vitesse et de la taille de la carte
      */
     private Point ouFuir () {
         //todo
-    } //congenereLePlusProche
+    } //ouFuir
 
 
-    /*FUIR - Comportement:
+    /**FUIR - Comportement:
       La proie regarde dans son champs de vision (getVision) si il y a un ou plusieurs prédateurs
       SI c'est le cas, ALORS elle se déplace dans la direction opposé au prédateur le plus proche
       SINON, elle ne fait rien
      */
-    private void fuir() {
-        if (isPredateurPresent ()) {
+    private void fuir(ArrayList <Espece> Predateur) {
+        if (trouverIndividuProche (Predateur)) {
             allerVersPosition (ouFuir ());
         }
     } //fuir ()
 
-    /*PREDATEURPRESENT - Comportement:
-      Renvoie TRUE uniquement si au moins un prédateur est dans le champs de vision (getVision) de la proie
+    /**PREDATEURPRESENT - Comportement:
+     * Renvoie TRUE uniquement si au moins un prédateur est dans le champs de vision (getVision) de la proie
      */
     private boolean isPredateurPresent () {
         //todo
     } //predateurPresent
 
 
-    /*CONGENERELEPLUSPROCHE - Comportement:
-      Parcourt le tableau d'espèce congénère et retourne le point du congénère le plus proche
-     */
-    private Point congenereLePlusProche () {
-        //todo
-    } //congenereLePlusProche
-
-
-    /*ALLERVERSCONGENERE - Comportement:
-      SI il y des congénères (aka même espèce),
-      ALORS la créature se déplace vers eux et se reproduit SI elle ne l'a pas déjà fait
-      SINON, elle ne fait rien
+    /**ALLERVERSCONGENERE - Comportement:
+     * SI il y des congénères (aka même espèce),
+     * ALORS la créature se déplace vers eux et se reproduit SI elle ne l'a pas déjà fait
+     * SINON, elle ne fait rien
      */
     private void allerVersCongenere (this.espece) {
         if (isCongenerePresent ()) {
@@ -61,18 +56,18 @@ public class Proie extends Espece {
     }
 
 
-    /*JOUERTOUR - Comportement:
-      SI un prédateur ou plus est dans son champs de vision,
-      ALORS la proie fuit,
-      SINON SI une autre proie ou plus est dans son champs de vision,
-      ALORS elle se reproduit,
-      SINON, elle ne fait rien
+    /**JOUERTOUR - Comportement:
+     * SI un prédateur ou plus est dans son champs de vision,
+     * ALORS la proie fuit,
+     * SINON SI une autre proie ou plus est dans son champs de vision,
+     * ALORS elle se reproduit,
+     * SINON, elle ne fait rien
      */
-    public void jouerTour() {
+    public void jouerTour(ArrayList <Espece> Proie, ArrayList <Espece> Predateur) {
         this.age += 1;
-        seReproduire ();
-        fuir();
-        allerVersCongenere();
+        seReproduire (Proie);
+        fuir (Predateur);
+        allerVersCongenere (Proie);
     } //jouerTour ()
 
 }
